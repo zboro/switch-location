200fed4ef885d7b80f28c2bcec0f410ab742e779
Lukas Zboron <zboron.lukas@gmail.com>
2015-10-02 06:40:24 +0200

    added new tab and incognito buttons

--- popup.js
+++ popup.js
@@ -18,15 +18,34 @@
 
 	function renderRule(rule) {
 		var liNode = document.createElement("li");
-		var switchButton = createSwitchButton(rule);
-		liNode.appendChild(switchButton);
+		liNode.appendChild(createSwitchButton(rule));
+		liNode.appendChild(createNewTabButton(rule));
+		liNode.appendChild(createIncognitoButton(rule));
 		rulesList.appendChild(liNode);
 	}
 
 	function createSwitchButton(rule) {
 		var button = document.createElement("button");
 		button.appendChild(document.createTextNode(rule.name));
+		button.className = "switchButton";
+		button.title = "Switch location";
 		button.onclick = switchPage.bind(null, rule);
+		return button;
+	}
+
+	function createNewTabButton(rule) {
+		var button = document.createElement("button");
+		button.className = "newTabBtn";
+		button.innerHTML = "+";
+		button.title = "Open in new tab";
+		button.onclick = openNewTab.bind(null, rule);
+		return button;
+	}
+
+	function createIncognitoButton(rule) {
+		var button = document.createElement("button");
+		button.className = "incognitoBtn";
+		button.onclick = openIncognito.bind(null, rule);
 		return button;
 	}
 
@@ -36,6 +55,27 @@
 		}, function(tabs) {
 			chrome.tabs.update(tabs[0].id, {
 				url: tabs[0].url.replace(new RegExp(rule.pattern), rule.replacement)
+			});
+		});
+	}
+
+	function openNewTab(rule) {
+		chrome.tabs.query({
+			"active": true
+		}, function(tabs) {
+			chrome.tabs.create({
+				url: tabs[0].url.replace(new RegExp(rule.pattern), rule.replacement)
+			});
+		});
+	}
+
+	function openIncognito(rule) {
+		chrome.tabs.query({
+			"active": true
+		}, function(tabs) {
+			chrome.windows.create({
+				"url": tabs[0].url.replace(new RegExp(rule.pattern), rule.replacement),
+				"incognito": true
 			});
 		});
 	}
